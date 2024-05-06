466d8e8355356a87603aa35c4df87fea6b9860a0
Miguel Sinay <mmviniciosinay@gmail.com>
2024-05-05 18:57:49 -0600

    Router applied

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express()
 
 const {infoCursos} = require('./cursos.js')
 
+// Routers
+const routerProgramacion = express.Router()
+app.use('/api/cursos/programacion', routerProgramacion)
+const routerMatematicas = express.Router()
+app.use('/api/cursos/matematicas', routerMatematicas)
+
 // Routing
 
 app.get('/', (req, res) => {
@@ -15,17 +21,17 @@ app.get('/api/cursos', (req, res) => {
 
 // Programación
 
-app.get('/api/cursos/programacion', (req, res) => {
+routerProgramacion.get('/', (req, res) => {
     res.send(JSON.stringify(infoCursos.programacion))
 })
-app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
+routerProgramacion.get('/:lenguaje', (req, res) => {
     const lenguaje = req.params.lenguaje
     const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje)
     if (resultados.length === 0) return res.status(404).send(`No se encontraron cursos de ${lenguaje}`)
     if (req.query.ordenar === 'vistas') return res.send(JSON.stringify((a, b) => b.vistas - a.vistas))
     res.send(JSON.stringify(resultados))
 })
-app.get('/api/cursos/programacion/:lenguaje/:nivel', (req, res) => {
+routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
     const lenguaje = req.params.lenguaje
     const nivel = req.params.nivel
     const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel)
@@ -35,10 +41,10 @@ app.get('/api/cursos/programacion/:lenguaje/:nivel', (req, res) => {
 
 // Matemáticas
 
-app.get('/api/cursos/matematicas', (req, res) => {
+routerMatematicas.get('/', (req, res) => {
     res.send(JSON.stringify(infoCursos.matematicas))
 })
-app.get('/api/cursos/matematicas/:tema', (req, res) => {
+routerMatematicas.get('/:tema', (req, res) => {
     const tema = req.params.tema
     const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema)
     if (resultados.length === 0) return res.status(404).send(`No se encontraron cursos de ${tema}`)
